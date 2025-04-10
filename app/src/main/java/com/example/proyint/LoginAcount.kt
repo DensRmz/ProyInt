ea7dd531a5150f46954a551b638db5265ab448ec
Omar <omaaramirez@gmail.com>
2025-04-10 15:49:24 -0600

    login

--- app/src/main/java/com/example/proyint/LoginAcount.kt
+++ app/src/main/java/com/example/proyint/LoginAcount.kt
@@ -1,32 +1,20 @@
 package com.example.proyint
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Login : AppCompatActivity() {
+class LoginAcount : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_login_acount)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btn: Button = findViewById(R.id.buttonLogin)
-        btn.setOnClickListener {
-
-            val intent: Intent = Intent(this, LoginAcount:: class.java)
-            startActivity(intent)
-        }
-
-
-
     }
-
 }
